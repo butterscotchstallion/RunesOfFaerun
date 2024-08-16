155567735e3e9fc855086443552e29bd71a5ed5b
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-08-16 02:13:17 -0400

    Comments QuestHandler init

--- RunesOfFaerun/Mods/RunesOfFaerun/ScriptExtender/Lua/Server/EventHandler.lua
+++ RunesOfFaerun/Mods/RunesOfFaerun/ScriptExtender/Lua/Server/EventHandler.lua
@@ -12,7 +12,7 @@ local spellStealSuccessCasted = false
 
 local function OnSessionLoaded()
     RunesOfFaerun.Utils.PrintVersionMessage()
-    RunesOfFaerun.QuestHandler.Initialize()
+    --RunesOfFaerun.QuestHandler.Initialize()
 end
 
 local function OnEnteredLevel(templateName, rootGUID, level)
